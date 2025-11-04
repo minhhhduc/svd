1addf500ff97b36b9619ef0071ef377e0a850040
DangTAnh <23001821@hus.edu.vn>
2025-11-04 11:38:52 +0700

    new build and make

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
-mingw32-make all
+mingw32-make clean
+mingw32-make all USE_OPENCL=0
+if %ERRORLEVEL% neq 0 (
+    echo Build failed!
+    exit /b 1
+)
+echo Build successful! Running main program...
 .\bin\main.exe
